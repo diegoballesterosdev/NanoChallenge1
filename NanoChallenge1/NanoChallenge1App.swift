f497721d9770771936870a1adf5e2bc0247cd465
diegoballesterosdev <diegoballesteros.dev@gmail.com>
2023-11-14 11:33:55 +0100

    Initial Commit

---
+++ NanoChallenge1/NanoChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge1App.swift
+//  NanoChallenge1
+//
+//  Created by Diego Ballesteros on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
